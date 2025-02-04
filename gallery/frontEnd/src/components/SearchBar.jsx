aa38e8a0f90c5ab574ccf762530c279b5cef4268
Khaleel05 <116633482+Khaleel05@users.noreply.github.com>
2025-02-04 22:23:13 +0000

    added search engin functionallity ait is able to generate a list of movies and displays the list of movie objects in the console.

--- gallery/frontEnd/src/components/SearchBar.jsx
+++ gallery/frontEnd/src/components/SearchBar.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './searchBar.css'
 
 function SearchBar() {
+  //Track the input
+  const [input, setInput] = useState();
+  //Track  the list of movies generated
+  const [searchedMovies, setSerachedMovies] = useState([]);
+
+  useEffect (() => {
+    const fetchData = async () => {
+      if(input.length > 2){
+        //Fetch data from the API
+        try{
+          const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ac8e81688a0f465351ee8afbfd35c253&query=${input}`);
+          setSerachedMovies(response.data.results);
+        }
+        catch(error){
+          console.error(error);
+        }
+      }
+    };
+    const timer = setTimeout(() => {
+      fetchData();
+    }, 500);  // Adding a debounce time of 500ms
+
+    console.log(searchedMovies);
+
+    return () => clearTimeout(timer);  // Clean up the timer
+  }, [input]);
+  
+   
   return (
     <div className='search'>
-      <input type='text' placeholder='Search'/>
+      <input type='text' placeholder='Search' value={input} onChange={(e)=> setInput(e.target.value)}/>
       <ion-icon name = 'search-outline'></ion-icon>
     </div>
   )
