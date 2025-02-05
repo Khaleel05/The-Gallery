4e61324915522aa0d195e5f60a85d7750b2cf2f6
Khaleel05 <116633482+Khaleel05@users.noreply.github.com>
2025-02-05 09:47:05 +0000

    sorted out the scroll buton on the carousel and also fixed the bug in the srach bar setting the input to an empty string

--- gallery/frontEnd/src/sections/Carousel.jsx
+++ gallery/frontEnd/src/sections/Carousel.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Movies from '../API/movies'
 import Style from './carousel.module.css'
 
 //the component containing the movies and lining them up. 
 const Carousel = () => {
+
+
+  const carouselRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: -300, behavior: 'smooth' });
+    }
+  };
+
+  const scrollRight = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: 300, behavior: 'smooth' });
+    }
+  };
+
   return (
     <div style={{border:'solid 1px blue'}}>
       <h2 style={{color: 'white', fontFamily: 'sans-serif', marginLeft: '1.2em'}}>New releases</h2>
       <div style={{boarder: 'solid 1px blue'}}>
-          <div  className={Style.carousel}>
-              <ion-icon style={{color: 'white', height:'70px', width:'70px', position: 'absolute', left: '2%'}} name="arrow-back-circle-outline"></ion-icon>
-              <ion-icon style={{color: 'white', height:'70px', width:'70px', position: 'absolute', right: '2%'}}  name="arrow-forward-circle-outline"></ion-icon>
+          <div  className={Style.carousel} ref={carouselRef}>
+              <ion-icon style={{color: 'white', height:'70px', width:'70px', position: 'absolute', left: '2%', cursor:'pointer'}} name="arrow-back-circle-outline" onClick={scrollLeft}></ion-icon>
+              <ion-icon style={{color: 'white', height:'70px', width:'70px', position: 'absolute', right: '2%', cursor:'pointer'}}  name="arrow-forward-circle-outline" onClick={scrollRight}>></ion-icon>
           <Movies/>
           </div>
       </div>
